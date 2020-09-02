ab5be5412a12ba09cf88d8363155e85720e3d001
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-02 17:03:00 -0500

    CRUD- Crear un nuevo usuario, modificar y borrar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import * as CRUD from './js/crud-provider';
 
 CRUD.getUsuario(2).then(console.log);
+
+CRUD.crearUsuario({
+    name: 'Marco',
+    job: 'Ingeniero'
+}).then(console.log);
+
+CRUD.actualizarUsuario(1,{
+    name: 'Rafael',
+    job: 'Carpintero'
+}).then(console.log);
+
+CRUD.borrarUsuario(1).then(console.log);
--- src/js/crud-provider.js
+++ src/js/crud-provider.js
@@ -8,6 +8,41 @@ const getUsuario = async(id) => {
     return data;
 };
 
+const crearUsuario = async(usuario) => {
+    const resp = await fetch(urlCRUD, {
+        method: 'POST',
+        body: JSON.stringify(usuario),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    // console.log(await resp.json());
+    return await resp.json();
+};
+
+const actualizarUsuario = async(id,usuario) => {
+    const resp = await fetch(`${urlCRUD}/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(usuario),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    return await resp.json();
+};
+
+const borrarUsuario = async(id) => {
+    const resp = await fetch(`${urlCRUD}/${id}`,{
+        method: 'DELETE'
+    });
+
+    return (resp.ok) ? 'Borrado' : 'No se pudo eliminar';
+};
+
 export {
-    getUsuario
+    getUsuario,
+    crearUsuario,
+    actualizarUsuario,
+    borrarUsuario
 }
