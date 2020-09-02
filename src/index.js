b1a78bf8fc8a44e1f93004abbce5ec06af58da0a
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-02 11:42:00 -0500

    Pro tip: Promesas en cadena

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 
-fetch(jokeUrl).then(resp => {
-  // resp.json().then(data => {
-  //   console.log(data.id);
-  //   console.log(data.value);
-  // });
-    //Con restructuración
-  resp.json().then(({id, value}) => {
-    console.log(id);
-    console.log(value);
-  });
-});
+// fetch(jokeUrl).then(resp => {
+//   // resp.json().then(data => {
+//   //   console.log(data.id);
+//   //   console.log(data.value);
+//   // });
+//     //Con restructuración
+//   resp.json().then(({id, value}) => {
+//     console.log(id);
+//     console.log(value);
+// });
+// });
+
+/*
+Promesas en cadena
+*/
+
+fetch(jokeUrl)
+    .then(resp => resp.json())
+    .then(({id,value}) => {
+        console.log(id,value);
+    });
+    
