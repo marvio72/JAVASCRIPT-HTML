8536ca5349e05047ca3175fc49a9e251529c10e8
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-02 15:48:15 -0500

    Crear una tabla de usuarios

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-// import { init } from './js/chistes-page';
+// import { init as initChistes} from './js/chistes-page';
+import { init } from './js/usuarios-page';
 import { obtenerUsuarios } from './js/http-provider';
 
 
-// init();
+init();
 
 obtenerUsuarios().then(console.log);
