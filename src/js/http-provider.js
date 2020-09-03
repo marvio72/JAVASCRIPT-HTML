eee9dd4d015d2099d4a810778a20e81f6c10c8a7
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-09-03 16:40:16 -0500

    Subir imagen a Cloudinary

--- src/js/http-provider.js
+++ src/js/http-provider.js
@@ -1,6 +1,10 @@
 const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 const urlUsuarios = 'https://reqres.in/api/users?page=2';
 
+//Cloudinary
+const cloudPreset = 'okk1bb3l';
+const cloudUrl = 'https://api.cloudinary.com/v1_1/marvio72/upload';
+
 const obtenerChiste = async() => {
 
     try {
@@ -32,7 +36,34 @@ const obtenerUsuarios = async() => {
     
 };
 
+//ArchivoSubir :: File
+const subirImagen = async(archivoSubir) => {
+    
+    const formData = new FormData();
+    formData.append('upload_preset', cloudPreset);
+    formData.append('file', archivoSubir);
+
+    try {
+        const resp = await fetch(cloudUrl,{
+            method: 'POST',
+            body: formData
+        });
+
+        if(resp.ok){
+            const cloudResp = await resp.json();
+            // console.log(cloudResp);
+            return cloudResp.secure_url;
+        } else {
+            throw await resp.json();
+        }
+        
+    } catch (error) {
+        throw error;
+    }
+};
+
 export{
     obtenerChiste,
-    obtenerUsuarios
+    obtenerUsuarios,
+    subirImagen
 };
